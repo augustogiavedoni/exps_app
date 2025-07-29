dcb7252a183c730e49d06a21320fb54cda4b14e6
Augusto Giavedoni <augusto.giavedoni@gmail.com>
2025-07-29 19:57:03 -0300

    chore(app): register repository layer

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,9 +1,28 @@
+import 'package:expenses_repository/expenses_repository.dart';
 import 'package:exps_app/counter/counter.dart';
 import 'package:exps_app/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({
+    super.key,
+    required ExpensesRepository expensesRepository,
+  }) : _expensesRepository = expensesRepository;
+
+  final ExpensesRepository _expensesRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _expensesRepository,
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
