e5a32f1dbe93084ea5cfa4b5226a7f90ed8dd2ae
Augusto Giavedoni <augusto.giavedoni@gmail.com>
2025-07-29 19:59:03 -0300

    test(app): adapt test cases to recent changes

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,28 @@
+import 'package:expenses_repository/expenses_repository.dart';
 import 'package:exps_app/app/app.dart';
 import 'package:exps_app/counter/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _MockExpensesRepository extends Mock implements ExpensesRepository {}
 
 void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+  group('$App', () {
+    late ExpensesRepository expensesRepository;
+
+    setUp(() {
+      expensesRepository = _MockExpensesRepository();
     });
+
+    testWidgets(
+      'Should render the $CounterPage widget.',
+      (tester) async {
+        await tester.pumpWidget(
+          App(expensesRepository: expensesRepository),
+        );
+
+        expect(find.byType(CounterPage), findsOneWidget);
+      },
+    );
   });
 }
